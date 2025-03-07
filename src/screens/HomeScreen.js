0a738b6ff596a93446ef3c0e85da7b5654f7aaad
Shterion <shterion.stamboliev@gmail.com>
2025-03-07 10:09:22 +0200

    added box screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -33,6 +33,10 @@ const HomeScreen = ({ navigation }) => {
         onPress={() => navigation.navigate("TextScreen")}
         title="Go to Text Screen"
       />
+      <Button
+        onPress={() => navigation.navigate("BoxScreen")}
+        title="Go to Box Screen"
+      />
     </View>
   );
 };
